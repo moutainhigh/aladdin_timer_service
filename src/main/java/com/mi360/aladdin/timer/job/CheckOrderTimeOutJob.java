bd6eeaff44073725a5172ea5d586fcf85ef6511d
JustArgo <565148812@qq.com>
2016-05-10 16:29:16 +0800

    更新配置

--- src/main/java/com/mi360/aladdin/timer/job/CheckOrderTimeOutJob.java
+++ src/main/java/com/mi360/aladdin/timer/job/CheckOrderTimeOutJob.java
@@ -11,8 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.mi360.aladdin.entity.order.Order;
-import com.mi360.aladdin.mapper.order.OrderMapper;
-import com.mi360.aladdin.mq.base.producer.MqProducer;
+import com.mi360.aladdin.mq.service.MqService;
 import com.mi360.aladdin.order.service.IOrderService;
 
 @Component
@@ -24,10 +23,10 @@ public class CheckOrderTimeOutJob {
 	private IOrderService orderService;
 	
 	@Autowired
-	private MqProducer mqProducer;
+	private MqService mqService;
 	
 	
-	@Scheduled(cron="0/10 * * * * ? ")
+	@Scheduled(cron="0 0/1 * * * ? ")
 	public void cancelPayTimeOutOrder(){
 		
 		String requestId = UUID.randomUUID().toString().replace("-", "");
@@ -46,7 +45,7 @@ public class CheckOrderTimeOutJob {
 	 * 	判断订单是否距离下单时间超过15天 每隔5分钟到数据库检车一次 
 	 * 	1 如果已发货 则自动改为 COM 并且退款状态 不为 退款中或  申请退款  
 	 */
-	@Scheduled(cron="0/5 * * * * ? ")
+	@Scheduled(cron="0 0/5 * * * ? ")
 	public void autoCompleteOrder(){
 		
 		String requestId = UUID.randomUUID().toString().replace("-", "");
@@ -72,24 +71,26 @@ public class CheckOrderTimeOutJob {
 		//TODO 判断退货的问题  如果该子订单 有订单商品 处于 以下5种 状态 则不能已完成
 			/* 1 申请退货
 			 * 2 退货审核通过
-			 * 3 
-			 * 4 退货中
-			 * 5 待退款
-			 * 6 退款中 
+			 * 3 退货中
+			 * 4 待退款
+			 * 5 退款中 
 			 */
 		
 		List<Order> orderList = orderService.selectOvertimeOrder(requestId);
 		for(int i=0;i<orderList.size();i++){
 			
 			//拿到的子订单 是 未申请退款的或退款失败的  或  退款审核被拒绝的  现在要 判断是否有退货的
-			//Long count = orderService.selectCanNotCompleteReturnGoodsCount(requestId, orderList.get(i).getID());
+			Long count = orderService.selectCanNotCompleteReturnGoodsCount(requestId, orderList.get(i).getID());   //此处用orderCode 而不是ID  是为了兼容 分库 分表 时  只保证 orderCode唯一 不保证 orderID唯一
 			
-			orderList.get(i).setOrderStatus("COM");
-			orderList.get(i).setConfirmTime(new Date(orderList.get(i).getCreateTime().getTime()+ 15 * 24 * 60 * 60 * 1000));
-			logger.info("订单 "+orderList.get(i).getOrderCode()+" 已完成");
-			orderService.updateOrder(orderList.get(i), requestId);
-			mqProducer.orderComplete(requestId, orderList.get(i).getOrderCode(), orderList.get(i).getConfirmTime().getTime());
-			
+			if(count>0){//表明该子订单   现在不能改为已完成  因为还有订单商品处于退货状态中 
+				
+			}else{
+				orderList.get(i).setOrderStatus("COM");
+				orderList.get(i).setConfirmTime(new Date(orderList.get(i).getCreateTime().getTime()+ 15 * 24 * 60 * 60 * 1000));
+				logger.info("订单 "+orderList.get(i).getOrderCode()+" 已完成");
+				orderService.updateOrder(orderList.get(i), requestId);
+				mqService.orderComplete(requestId, orderList.get(i).getOrderCode(), orderList.get(i).getConfirmTime().getTime());
+			}
 			
 		}
 		
